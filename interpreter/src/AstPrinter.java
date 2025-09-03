76a4ed8fc1eb0d345b53987b50dd8e376dc0b5fe
Mircea Cadariu <cadariu.mircea@gmail.com>
2025-09-03 20:33:18 +0100

    Add expressions and AstPrinter

---
+++ interpreter/src/AstPrinter.java
@@ -0,0 +1,52 @@
+class AstPrinter implements Expr.Visitor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return paranthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return paranthesize("group", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return paranthesize(expr.operator.lexeme, expr.right);
+    }
+
+    private String paranthesize(String name, Expr... exprs) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("(").append(name);
+
+        for (Expr expr : exprs) {
+            stringBuilder.append(" ");
+            stringBuilder.append(expr.accept(this));
+        }
+
+        stringBuilder.append(")");
+
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        Expr expression = new Expr.Binary(
+                new Expr.Unary(
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(123)),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(new Expr.Literal(45.67)));
+
+        System.out.println(new AstPrinter().print(expression));
+    }
+}
